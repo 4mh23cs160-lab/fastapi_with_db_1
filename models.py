2e8240a44ce6fade63de28903a978250a64a8152
sahana <4mh13cs130a@gmail.com>
2026-02-06 15:15:02 +0530

    fastapi

--- models.py
+++ models.py
@@ -5,6 +5,5 @@ class user(base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    email = Column(String, unique=True, index=True)
     password = Column(String)
     
