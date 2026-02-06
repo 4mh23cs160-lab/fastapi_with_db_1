67a39848c4c749f41923c97a8792e297507138d0
sahana <4mh13cs130a@gmail.com>
2026-02-06 15:04:01 +0530

    fastapi

---
+++ db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+base = declarative_base()
+
+DATABASE_URL = "sqlite:///./test.db"
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ models.py
@@ -0,0 +1,9 @@
+from db import base
+
+class user(base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    
